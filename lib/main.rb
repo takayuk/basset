e919b025b7a414d541f27b9d8c86702cb492d55c
takayuk <thiscall.rd@gmail.com>
2010-11-05 13:29:00 +0900

    Add users

--- lib/main.rb
+++ lib/main.rb
@@ -8,22 +8,25 @@ Mongoid.configure {|conf|
   conf.master = Mongo::Connection.new("133.83.90.36", 27017).db("flickr")
 }
 
+=begin
 open("user.list"){|f|
   while l=f.gets
     #@user = Schema::User.new(:user_id => l.chomp)
     #@user.save
   end
 }
-
-=begin
-Schema::User.find(:user_id).each{|r|
-
-  r.update_attributes!(:contact_id => %w!contact_a contact_b!)
-}
 =end
 
+Schema::User.find(:user_id).each{|r|
+
+  #r.update_attributes!(:contact_id => %w!contact_a contact_b!)
+  r.destroy
+}
+
+=begin
 Schema::User.find(:user_id).each{|u|
   puts "#{u.user_id}, #{u.contact_id}"
 }
+=end
 p Schema::User.find(:user_id).count
 
