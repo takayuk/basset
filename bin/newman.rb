d5d9816292337ab46c77c7724073803bd84d87c3
kamei <kamei@ubuntu.(none)>
2010-12-13 03:50:56 +0900

    [Ehc] Calculate @m with one-line

--- bin/newman.rb
+++ bin/newman.rb
@@ -56,18 +56,16 @@ end
   @lmat=Array.new(@vsize){Hash.new(0)}
   @gv.each_with_index{|vv,i|
     @th[vv].each_with_index{|lw,j|
-      #@lmat[i].store(@gv.index(lw[0]),lw[1].to_f)
-      @lmat[i].store(@gv.index(lw[0]),1.0)
+      @lmat[i].store(@gv.index(lw[0]),lw[1].to_f)
+      #@lmat[i].store(@gv.index(lw[0]),1.0)
     }
   }
 
-  @m=0.0
-  #@lmat.each{|v|@m+=v.values.size/2}
-  @lmat.each{|v|@m+=v.keys.size}
-  @m.freeze
+  @m=@lmat.map{|v|v.values.to_a}.flatten.inject(0){|t,a|t+a}.freeze
 
   @cluster=Array.new(@vsize){|v|[v]}
 
+  @qmax=-10000000
   while @cluster.size > 1
     @e_=Array.new(@cluster.size){Hash.new(0)}
     for i in 0..@cluster.size-1
@@ -94,81 +92,30 @@ end
         @dqmax=[i,j,@dq] if @dqmax[2] < @dq
       }
     end
-    p @dqmax
 
-    @cluster[@dqmax[0]].each{|vi|
-      @cluster[@dqmax[1]].each{|vj|
-        @lmat[vi][vj]=@lmat[vj][vi]=0
-      }
-    }
-    @cluster[@dqmax[1]]+=@cluster[@dqmax[0]]
-    @cluster.delete_at(@dqmax[0])
-    @q=0
-    for i in 0..@cluster.size-1
-      @e_[i].each{|j,w|
-        @q+=@e_[i][i]-(@a_[i]**2)
-      }
-    end
-    p @q
-  end
-
-  break
-
-  @qmax=0
-  @last=[]
-  while @cluster.size>1
-
-    @e=Array.new(@cluster.size){Hash.new(0)}
-
-    @dqmax=[nil,nil,0]
-    @a=Array.new(@cluster.size,0.0)
-    for i in 0..@cluster.size-1
-      for j in 0..@cluster.size-1
-        #next if i==j
-        @w=0
-        @cluster[i].each{|vi|
-          @cluster[j].each{|vj|
-            @w+=@lmat[vi][vj]
-          }
+    begin
+      @cluster[@dqmax[0]].each{|vi|
+        @cluster[@dqmax[1]].each{|vj|
+          @lmat[vi][vj]=@lmat[vj][vi]=0
         }
-        @w/=@m
-    
-        @e[i].store(j,@w) if @w > 0.0
-       
-        @dqmax=[i,j,@w] if @dqmax[2] < @w
-       
-        @a[i]+=@w
+      }
+      @cluster[@dqmax[1]]+=@cluster[@dqmax[0]]
+      @cluster.delete_at(@dqmax[0])
+      @q=0
+      for i in 0..@cluster.size-1
+        @e_[i].each{|j,w|
+          @q+=@e_[i][i]-(@a_[i]**2)
+        }
       end
-    end
 
-    break unless @dqmax[2]>0.0
-    next if @dqmax[0]==@dqmax[1]
-
-    @cluster[@dqmax[1]]+=@cluster[@dqmax[0]]
-    @cluster.delete_at(@dqmax[0])
-    #p @cluster,@dqmax
-
-    @q=0.0
-    for i in 0..@cluster.size-1
-      for j in @e[i].keys
-        if i == j
-          @q+=@e[i][j] - (@a[i]**2)
-        end
-      end
-=begin
-      if @e[i][i] > 0.0
-        @q+=@e[i][i] - (@a[i]**2)
-      end
-=end
-    end
-    p @q
-    
-    if @qmax < @q
-      @qmax=@q
-      @last=@cluster
+      @qmax=@q if @qmax < @q
+    rescue
+      break
     end
   end
-  p @qmax,@last
+
+  p @qmax,@cluster
+
   break
 }
 
