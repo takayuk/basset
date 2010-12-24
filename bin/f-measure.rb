6efcefce3fc6f4c1a33ee66b462786eb07c3633f
kamei <kamei@corot.(none)>
2010-12-24 19:21:29 +0900

    [Ehc] f-measure

--- bin/f-measure.rb
+++ bin/f-measure.rb
@@ -2,24 +2,50 @@
 # -*- encoding: utf-8 -*-
 # -*- coding: utf-8 -*-
 
-@prh=Hash.new([].freeze)
-open("/home/kamei/workspace/dataset/kldiv").readlines.first(1).each{|v|
-  #v.chomp.split(" ").map{|w|{(@i+=1)=>w.to_f}}
-  v.chomp.split(" ").each_with_index{|w,i|@prh.store(i,w.to_f)}
+@user_ve=Hash.new(0)
+open("/home/kamei/workspace/dataset/user_ve").readlines.map{|v|
+  v.chomp
+  vv=v.split(" ").map{|w|w.gsub(/:[1-9]/,"")}
+  @user_ve.store(vv.shift,vv)
 }
 
-@ulist=open("/home/kamei/workspace/dataset/kldiv-userlist").readlines.map{|v|v.chomp}
+@ulist=Hash.new(0)
+open("/home/kamei/workspace/dataset/kldiv-userlist").readlines.each_with_index{|v,i|
+  @ulist.store(i,v.chomp)
+}
 
-@min=100000.0
-@esti=nil
-@prh.sort{|a,b|a[1]<=>b[1]}.each{|k,v|
+@pr=Array.new(@ulist.size).map!{|v|v=Array.new(@ulist.size,0)}
+open("/home/kamei/workspace/dataset/kldiv").readlines.each_with_index{|v,i|
+  v.chomp.split(" ").each_with_index{|w,j|
+    @pr[i][j]=w.to_f
+  }
+}
 
-  if v < @min && v > 0
-    @min=v
-    @esti=k
+@sample=nil
+@ulist.each{|i,u|
+  if @user_ve[u]!=0
+    @sample=[i,u]
+    break
   end
 }
-p @esti
-p @ulist.first(10)
-p @ulist[@esti]
+
+$min=100000000
+@esti=[]
+@pr[@sample[0]].each_with_index{|v,i|
+  if v < 10 && v > 0
+    @esti << i
+  end
+}
+#@kl.map!{|v|v*-1 if v < 0}.sort!
+=begin
+@pr[@sample[0]].each_with_index{|j,id|
+  if j > 0 && j < $min
+    $min=j
+    @esti=id
+  end
+}
+=end
+@esti.each{|e|
+  p @user_ve[@sample[1]].include?(@ulist[e])
+}
 
