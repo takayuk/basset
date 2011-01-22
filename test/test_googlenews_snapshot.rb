6e6368e0ecca37d781f5b3bb89d045aeb73bb0fc
kamei <thiscall.rd@gmail.com>
2011-01-22 15:06:35 +0900

    [Ehc] Google News data extracted

--- test/test_googlenews_snapshot.rb
+++ test/test_googlenews_snapshot.rb
@@ -6,6 +6,9 @@ $LOAD_PATH.push("/home/kamei/workspace/basset/bin")
 require"googlenews_snapshot.rb"
 
 @res=snapshot(start_index = 0)
-require"pp"
-pp @res
 
+require"pp"
+@res["responseData"]["results"].each{|v|
+  pp v["title"]
+  pp v["content"] unless (v["content"] =~ /[ぁ-ん]/).nil?
+}
