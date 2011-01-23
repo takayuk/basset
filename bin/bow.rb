04cae214b6fa7a519fd71ab6f35e7334dea43315
kamei <thiscall.rd@gmail.com>
2011-01-23 16:57:04 +0900

    [Ehc] Merge to bow.rb

--- bin/bow.rb
+++ bin/bow.rb
@@ -2,10 +2,26 @@
 # -*- encoding: utf-8 -*-
 # -*- coding: utf-8 -*-
 
-def bow sentence, parts_name
-  $LOAD_PATH.push("/home/kamei/workspace/basset/bin")
-  require"to_feature"
+def mecab sentence
+  require "MeCab"
+  
+  @result=Hash.new(0)
+  begin
+    @tagger=MeCab::Tagger.new("-Ochasen -d /home/kamei/local/lib/mecab/dic/ipadic-utf8")
+    @node = @tagger.parseToNode(sentence)
+    while @node do
+      @result.store(@node.surface.force_encoding("UTF-8"),
+                    @node.feature.force_encoding("UTF-8")) unless @node.surface.empty?
+      @node = @node.next
+    end
 
+  rescue
+    print "RuntimeError: ", $!, "\n";
+  end
+  @result
+end
+
+def bow sentence, parts_name
   @feature=Array.new
   unless (sentence =~ /[ぁ-ん]/).nil?
     @mecab_result=mecab(sentence)
@@ -17,4 +33,5 @@ def bow sentence, parts_name
   end
   @feature
 end
+
 
--- test/basset.rb
+++ test/basset.rb
@@ -4,24 +4,9 @@
 
 $LOAD_PATH.push("/home/kamei/workspace/basset/bin")
 require"googlenews_snapshot.rb"
-require"to_feature"
-
 @res=snapshot(start_index = 0)
 
-def bow sentence, parts_name
-  @feature=Array.new
-  unless (sentence =~ /[ぁ-ん]/).nil?
-    @mecab_result=mecab(sentence)
-    @mecab_result.each{|k,v|
-      next if v.nil? or v.empty?
-      @scanned = v.scan(/#{parts_name}/)
-      @feature << k if !@scanned.nil? and !@scanned.empty?
-    }
-  end
-  @feature
-end
-
-require"pp"
+require"bow"
 @res["responseData"]["results"].each{|v|
   p @f = bow(v["title"], "名詞")
 }
