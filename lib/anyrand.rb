ece62938156fbb2e8a7cd8c7ae4cd4d984bc5570
kamei <thiscall.rd@gmail.com>
2011-02-13 17:49:34 +0900

    [Ehc] Debugged rand_dirichlet with BigDecimal.

--- lib/anyrand.rb
+++ lib/anyrand.rb
@@ -2,7 +2,9 @@
 # -*- encoding: utf-8 -*-
 # -*- coding: utf-8 -*-
 
-def prob_normal avg,var
+require"bigdecimal"
+
+def rand_normal avg,var
   @a=Math::sqrt(Math::log(4.0)).freeze
   @b=Math::sqrt(2.0*Math::PI).freeze
   @s=(@a/(@b-@a)).freeze
@@ -48,7 +50,7 @@ def prob_normal avg,var
   (avg + (var * @y))
 end
 
-def prob_gamma alpha,beta
+def rand_gamma alpha,beta
   @d=(alpha.to_f - (1.0/3.0)).freeze
   begin
     @c=(1.0 / Math::sqrt(9.0 * @d)).freeze
@@ -62,7 +64,7 @@ def prob_gamma alpha,beta
     @v=0.0
     @z=nil
     while @v <= 0.0
-      @z=prob_normal(0.0,1.0)
+      @z=rand_normal(0.0,1.0)
       @v=(1.0 + (@c*@z))
     end
 
@@ -81,42 +83,20 @@ def prob_gamma alpha,beta
   @x
 end
 
-def prob_dirichlet alpha
+def rand_dirichlet alpha
   @k=alpha.size.freeze
 
-  @y=0.0
-  @y_i=Array.new(@k)
+  @y=Array.new(@k)
   for i in 0..(@k-1)
-    @y_i[i]=prob_gamma(alpha[i],1.0)
-    @y+=@y_i[i]
+    @y[i]=BigDecimal::new(rand_gamma(alpha[i],1.0).to_s)
   end
-  p @y_i,@y
-  @y.freeze
 
-  @x_i=Array.new(@k)
+  @ytotal = @y.inject(0){|sum,i|sum+i}.to_f.freeze
+
+  @x=Array.new(@k)
   for i in 0..(@k-1)
-    @x_i[i]=(@y_i[i]/@y)
+    @x[i]=@y[i]/@ytotal
   end
-  @x_i
+  @x
 end
 
-
-round = Proc.new{|n,d|(n*10**d).round / 10.0**d}
-
-#@h=Hash.new(0)
-open("dirichlet.txt","w"){|f|
-  10.times do 
-    #@h[round.call(prob_normal(0.0, 1.0), 2)]+=1
-    #@h[round.call(prob_gamma(0.4,1.0),2)]+=1
-    #@h[round.call(prob_gamma(2.0,1.0),2)]+=1
-    #@h[round.call(prob_gamma(4.0,1.0),2)]+=1
-    p @d=prob_dirichlet([5.0,5.0,2.0])
-    #f.puts "#{round.call(@d[0],2)},#{round.call(@d[1],2)},#{round.call(1.0-@d[0]-@d[1],2)}"
-    f.puts "#{round.call(@d[0],2)},#{round.call(@d[1],2)},#{round.call(@d[2],2)}"
-  end
-}
-=begin
-open("gamma.csv","w"){|f|
-  @h.each{|k,v| f.puts "#{k},#{v}"}
-}
-=end
