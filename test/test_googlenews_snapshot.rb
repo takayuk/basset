e737aa0becd7990adaed991fe240a1b3ade80d93
kamei <thiscall.rd@gmail.com>
2011-02-20 17:33:29 +0900

    [Ehc] test googlenews snapshot.

--- test/test_googlenews_snapshot.rb
+++ test/test_googlenews_snapshot.rb
@@ -7,9 +7,28 @@ require"googlenews_snapshot.rb"
 
 @res=snapshot(start_index = 0)
 
+require 'uri'
 require"pp"
-@res["responseData"]["results"].each{|v|
-  pp v["title"] unless (v["content"] =~ /[ぁ-ん]/).nil?
-  pp v["content"] unless (v["content"] =~ /[ぁ-ん]/).nil?
-  puts ""
-}
+@res["responseData"]["results"].each do |v|
+  @headline=v["content"].split("")
+  @headline=@headline[0..@headline.size-12]
+  
+  @h=Hash.new(0)
+  open(URI.escape(v["unescapedUrl"])).readlines.each do |line|
+    next if @text.nil?
+    @text=line.strip.gsub!(/<("[^"]*"|\'[^\']*\'|[^\'">])*>/,"")
+    if not @text.empty?
+      @text.force_encoding("UTF-8") if @text.encoding.to_s=="ASCII-8BIT"
+      @h.store(@text,(@text.split("") & @headline).size)
+    end
+  end
+  @h.sort{|a,b|b[1]<=>a[1]}.first(2).each{|k,v|
+    p k
+  }
+  #pp v["relatedStories"]
+  #pp v["content"]
+  #pp v["title"] unless (v["content"] =~ /[ぁ-ん]/).nil?
+  #pp v["content"] unless (v["content"] =~ /[ぁ-ん]/).nil?
+  #puts ""
+end
+
